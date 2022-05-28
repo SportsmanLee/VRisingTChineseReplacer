13f0d9f383d71084301b600ba3c3f6068d2973c7
SPML <a22856830@gmail.com>
2022-05-28 23:23:35 +0800

    Change to print Chinese messages

--- VRisingTChineseReplacer/VRisingTChineseReplacer.cpp
+++ VRisingTChineseReplacer/VRisingTChineseReplacer.cpp
@@ -7,23 +7,25 @@
 
 int main()
 {
+    std::locale::global(std::locale(""));
+
     SteamGameRegistryFinder gameFinder;
     const auto wstrGamePath = gameFinder.FindGamePath(L"V Rising");
-    std::wcout << L"Found game path: " << wstrGamePath << std::endl;
+    std::wcout << L"�C���Ҧb��|: " << wstrGamePath << std::endl;
 
     const auto dstFilePath = std::filesystem::path(wstrGamePath + LR"(\VRising_Data\StreamingAssets\Localization\TChinese.json)");
     const auto srcFilePath = std::filesystem::path(L"TChinese.json");
     if (std::filesystem::exists(srcFilePath))
     {
         std::filesystem::copy_file(srcFilePath, dstFilePath, std::filesystem::copy_options::overwrite_existing);
-        std::wcout << L"File copied to path: " << dstFilePath << std::endl;
+        std::wcout << L"�c�����ɮפw�ƻs��: " << dstFilePath.wstring() << std::endl;
     }
     else
     {
-        std::wcerr << L"Cannot find file: " << srcFilePath << std::endl
-        << LR"(Please place "TChinese.json" beside this .exe file.)" << std::endl;
+        std::wcerr << L"�䤣���c�����ɮ�: " << srcFilePath.wstring() << std::endl
+            << LR"(�бN TChinese.json �m�󦹵{���Ҧb��Ƨ��C)" << std::endl;
     }
 
-    std::wcout << std::endl << L"Press any key to close this window . . .";
+    std::wcout << std::endl << L"��J������H�������� . . .";
     std::getchar();
 }
