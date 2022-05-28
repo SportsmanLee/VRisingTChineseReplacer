b15b0e973feb807342b7998169a73183e4f68e2f
SPML <a22856830@gmail.com>
2022-05-28 23:21:27 +0800

    Remove redundant methods

--- VRisingTChineseReplacer/SteamGameRegistryFinder.h
+++ VRisingTChineseReplacer/SteamGameRegistryFinder.h
@@ -11,7 +11,6 @@ public:
 
 private:
     std::wstring FindInstallationDir(const std::wstring& wstrLibraryPath, const std::wstring& wstrGameName);
-    std::wstring GetItemStringFromVDF(const std::wstring& wstrVDFPath, const std::wstring& wstrItemName);
 
     std::wstring                m_wstrSteamPath;
     std::wstring                m_wstrLibraryFolderVDFPath;
